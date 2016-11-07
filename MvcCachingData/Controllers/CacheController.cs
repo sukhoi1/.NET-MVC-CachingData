8819ea7074b8ed1f4eadd798287bd1d153283f2d
Anton Lygin <lygin@live.com>
2016-11-07 22:39:16 +0300

    v0.002 - initial commit, adjusted project names;

---
+++ MvcCachingData/Controllers/CacheController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace MvcState.Controllers
+{
+    public class CacheController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View((long?)HttpContext.Cache["pageLength"]);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PopulateCache() 
+        {
+            HttpResponseMessage result = await new HttpClient().GetAsync("http://apress.com");
+            HttpContext.Cache["pageLength"] = result.Content.Headers.ContentLength;
+            return RedirectToAction("Index");
+        }
+    }
+}
