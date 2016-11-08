6a598949a1eb522e0cf6ad99e0d4093998b06d47
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-08 15:34:48 +0300

    v0.006 - Cache, depend on another cached item example;

---
+++ MvcCachingData/Controllers/AnotherCachedController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace MvcState.Controllers
+{
+    public class AnotherCachedController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View((long?)HttpContext.Cache["pageLength"]);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PopulateCache()
+        {
+            HttpResponseMessage result = await new HttpClient().GetAsync("http://apress.com");
+
+            long? data = result.Content.Headers.ContentLength;
+            CacheDependency dependency = new CacheDependency(Request.MapPath("~/data.txt"));
+            HttpContext.Cache.Insert("pageLength", data, dependency);
+
+            DateTime dt = DateTime.Now;
+            CacheDependency timeStampDependency = new CacheDependency(null, new string[] { "pageLength" });
+            HttpContext.Cache.Insert("pageTimeStamp", dt, timeStampDependency);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
