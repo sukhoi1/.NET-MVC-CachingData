aaafc9eae870258350bccc0f2c868ae651081bbf
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-08 16:42:38 +0300

    v0.007 - cache, creating custom dependencies;

---
+++ MvcCachingData/Controllers/CustomDependencyController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Caching;
+using System.Web.Mvc;
+using MvcState.Infrastructure;
+
+namespace MvcState.Controllers
+{
+    public class CustomDependencyController : Controller
+    {
+        public ActionResult Index()
+        {
+            SelfExpiringData<long?> seData =
+                (SelfExpiringData<long?>) HttpContext.Cache["pageLength"];
+            return View(seData == null ? null : seData.Value);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PopulateCache()
+        {
+            HttpResponseMessage result = await new HttpClient().GetAsync("http://apress.com");
+            long? data = result.Content.Headers.ContentLength;
+            SelfExpiringData<long?> seData = new SelfExpiringData<long?>(data, 3);
+            HttpContext.Cache.Insert("pageLength", seData, seData);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCachingData/Infrastructure/SelfExpiringData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Caching;
+
+namespace MvcState.Infrastructure
+{
+    public class SelfExpiringData<T> : CacheDependency
+    {
+        private T dataValue;
+        private int requestCount = 0;
+        private int requestLimit;
+
+        public T Value
+        {
+            get
+            {
+                if (this.requestCount++ >= this.requestLimit)
+                {
+                    NotifyDependencyChanged(this, EventArgs.Empty);
+                }
+
+                return this.dataValue;
+            }
+        }
+
+        public SelfExpiringData(T data, int limit)
+        {
+            this.dataValue = data;
+            this.requestLimit = limit;
+        }
+    }
+}
