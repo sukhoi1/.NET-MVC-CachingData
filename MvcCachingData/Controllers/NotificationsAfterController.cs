934c85aba2c4d75c25a99b9fff67ce262d2ce697
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-08 19:03:30 +0300

    v0.009 - cache, notification after cache is ejected (info only);

---
+++ MvcCachingData/Controllers/NotificationsAfterController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Caching;
+using System.Web.Mvc;
+using MvcState.Infrastructure;
+
+namespace MvcState.Controllers
+{
+    public class NotificationsAfterController : Controller
+    {
+        public ActionResult Index()
+        {
+            SelfExpiringData<long?> seData =
+                (SelfExpiringData<long?>)HttpContext.Cache["pageLength"];
+            return View(seData == null ? null : seData.Value);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PopulateCache()
+        {
+            HttpResponseMessage result = await new HttpClient().GetAsync("http://apress.com");
+            long? data = result.Content.Headers.ContentLength;
+
+            SelfExpiringData<long?> seData = new SelfExpiringData<long?>(data, 3);
+            HttpContext.Cache.Insert("pageLength", seData, seData, Cache.NoAbsoluteExpiration, Cache.NoSlidingExpiration,
+                CacheItemPriority.Normal, HandleNotification);
+
+            return RedirectToAction("Index");
+        }
+
+        private void HandleNotification(string key, object data, CacheItemRemovedReason reason)
+        {
+            System.Diagnostics.Debug.WriteLine("Item {0} removed. ({1})", key, Enum.GetName(typeof(CacheItemRemovedReason), reason));
+        }
+    }
+}
