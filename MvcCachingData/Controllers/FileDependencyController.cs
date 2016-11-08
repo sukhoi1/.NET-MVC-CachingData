280fca3fc8e1ed15eb0a2ec833bb45dbe24606c5
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-08 14:19:07 +0300

    v0.005 - Cache, FileDependency example;

---
+++ MvcCachingData/Controllers/FileDependencyController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Caching;
+using System.Web.Mvc;
+
+namespace MvcState.Controllers
+{
+    public class FileDependencyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View((long?)HttpContext.Cache["pageLength"]);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> PopulateCache()
+        {
+            HttpResponseMessage result = await new HttpClient().GetAsync("http://apress.com");
+            long? data = result.Content.Headers.ContentLength;
+            CacheDependency dependency = new CacheDependency(Request.MapPath("~/data.txt"));
+            HttpContext.Cache.Insert("pageLength", data, dependency);
+            return RedirectToAction("Index");
+        }
+    }
+}
